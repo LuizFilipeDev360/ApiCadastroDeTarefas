c60859f8a6d1d0b25083dad077219014155a7098
Luiz Filipe <lf360.dev@gmail.com>
2026-02-12 13:12:36 -0300

    Excuindo Pacote que não Estava sendo usado

--- ApiCadastroDeTarefas/Program.cs
+++ ApiCadastroDeTarefas/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddIdentityApiEndpoints<AuthUser>().AddEntityFrameworkStores<Ta
 // Add services to the container.
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
-builder.Services.AddControllers().AddNewtonsoftJson();
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(option =>
